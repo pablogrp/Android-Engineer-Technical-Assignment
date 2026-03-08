2375465c9ff149772f7e2289c99837e607630477
pablogrp <alu0101496139@ull.edu.es>
2026-03-08 16:15:01 +0000

    API Key generated

---
+++ app/src/main/java/com/example/android_engineer_technical_assignment/data/Constans.kt
@@ -0,0 +1,7 @@
+package com.example.android_engineer_technical_assignment.data
+
+object Constants {
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
+    const val API_KEY = "340d2c729636cb6fcacf9945f7bde3b1"
+}
