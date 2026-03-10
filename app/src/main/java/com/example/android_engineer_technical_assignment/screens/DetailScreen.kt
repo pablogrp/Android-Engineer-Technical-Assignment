9ed7538e08c3ddde5e7dd9fcd71cb1d9f8fb7a69
pablogrp <alu0101496139@ull.edu.es>
2026-03-10 10:18:55 +0000

    Add: new cleaner structure

--- app/src/main/java/com/example/android_engineer_technical_assignment/screens/DetailScreen.kt
+++ app/src/main/java/com/example/android_engineer_technical_assignment/screens/DetailScreen.kt
@@ -1,4 +1,68 @@
 package com.example.android_engineer_technical_assignment.screens
 
-class DetailScreen {
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+
+@Composable
+fun DetailScreen(
+    title: String,
+    posterPath: String,
+    overview: String,
+    onBack: () -> Unit
+) {
+    Scaffold { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.headlineMedium
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            if (posterPath != "null") {
+                val imageUrl = "https://image.tmdb.org/t/p/w500/$posterPath"
+                AsyncImage(
+                    model = imageUrl,
+                    contentDescription = "Poster",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(350.dp)
+                        .clip(RoundedCornerShape(16.dp)),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                text = overview,
+                style = MaterialTheme.typography.bodyLarge
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = onBack
+            ) {
+                Text(text = "Return")
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/android_engineer_technical_assignment/screens/MovieScreen.kt
+++ app/src/main/java/com/example/android_engineer_technical_assignment/screens/MovieScreen.kt
@@ -1,4 +1,67 @@
 package com.example.android_engineer_technical_assignment.screens
 
-class MovieScreen {
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import com.example.android_engineer_technical_assignment.data.Movie
+import com.example.android_engineer_technical_assignment.data.RetrofitClient
+import com.example.android_engineer_technical_assignment.ui.components.MovieItem
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+
+
+@Composable
+fun MovieScreen(navController: NavHostController, modifier: Modifier = Modifier) {
+    var movieList by remember { mutableStateOf<List<Movie>>(emptyList()) }
+    var isLoading by remember { mutableStateOf(true) }
+    var isError by remember { mutableStateOf(false) }
+
+    LaunchedEffect(Unit) {
+        try {
+            val response = RetrofitClient.apiService.getMovies()
+            movieList = response.results
+            isLoading = false
+        } catch (e: Exception) {
+            isError = true
+            isLoading = false
+        }
+    }
+
+    Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        when {
+            isLoading -> CircularProgressIndicator()
+            isError -> Text(text = "Error.")
+            movieList.isEmpty() -> Text(text = "No movie data available.")
+            else -> {
+                LazyColumn(modifier = Modifier.fillMaxSize()) {
+                    items(movieList) { currentMovie ->
+                        MovieItem(
+                            movie = currentMovie,
+                            onSeeMoreClick = {
+                                val cleanPoster = currentMovie.posterpath?.removePrefix("/") ?: "null"
+                                val encodedOverview = URLEncoder.encode(
+                                    currentMovie.overview ?: "No description",
+                                    StandardCharsets.UTF_8.toString()
+                                )
+
+                                navController.navigate("more_info/${currentMovie.title}/$cleanPoster/$encodedOverview")
+                            }
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
