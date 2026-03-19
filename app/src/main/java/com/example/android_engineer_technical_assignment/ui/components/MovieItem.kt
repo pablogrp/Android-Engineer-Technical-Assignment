1386fc02ba97eb326fe04440a1e7787e076644ed
pablogrp <alu0101496139@ull.edu.es>
2026-03-19 12:45:42 +0000

    feat: animation when liking a movie

--- app/src/main/java/com/example/android_engineer_technical_assignment/ui/components/MovieItem.kt
+++ app/src/main/java/com/example/android_engineer_technical_assignment/ui/components/MovieItem.kt
@@ -1,5 +1,11 @@
 package com.example.android_engineer_technical_assignment.ui.components
 
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.Spring
+import androidx.compose.runtime.getValue
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -8,10 +14,13 @@ import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -99,10 +108,22 @@ fun MovieItem(
                     )
 
                     IconButton(onClick = onFavoriteClick) {
+                        val scale by animateFloatAsState(
+                            targetValue = if (isFavorite) 1.3f else 1.0f,
+                            animationSpec = spring(
+                                dampingRatio = Spring.DampingRatioMediumBouncy,
+                                stiffness = Spring.StiffnessLow
+                            )
+                        )
+
                         Icon(
                             imageVector = if (isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
                             contentDescription = "Favorite",
-                            tint = if (isFavorite) Color.Red else Color.Gray
+                            tint = if (isFavorite) Color.Red else Color.Gray,
+                            modifier = Modifier.graphicsLayer(
+                                scaleX = scale,
+                                scaleY = scale
+                            )
                         )
                     }
                 }
